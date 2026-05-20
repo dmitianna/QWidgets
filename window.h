dd78a27415d25319ccf56183859ae54621eb5ded
dmitianna <dmitianna@bk.ru>
2026-05-20 15:15:15 +0700

    Добавлен пример из методичики
    В h уже убран Codec так как в 5 версии его не используют, а в 6 его просто нет. Проще будет обрабатывать текст другими методами. cpp пока взят без изменений

--- window.h
+++ window.h
@@ -2,13 +2,40 @@
 #define WINDOW_H
 
 #include <QWidget>
+#include <QFrame>
+#include <QLabel>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QValidator>
 
-class Widget : public QWidget
+class Window : public QWidget
 {
     Q_OBJECT
 
 public:
-    Widget(QWidget *parent = nullptr);
-    ~Widget();
+    Window(QWidget *parent = nullptr);
+    ~Window() = default;
+protected:
+    //QTextCodec *codec;
+    QFrame *frame; // рамка
+    QLabel *inputLabel; // метка ввода
+    QLineEdit *inputEdit; // строчный редактор ввода
+    QLabel *outputLabel; // метка вывода
+    QLineEdit *outputEdit; // строчный редактор вывода
+    QPushButton *nextButton; // кнопка Следующее
+    QPushButton *exitButton; // кнопка Выход
+public slots:
+    void begin(); // метод начальной настройки интерфейса
+    void calc(); // метод реализации вычислений
+};
+
+class StrValidator:public QValidator // класс компонента проверки ввода
+{
+public:
+    StrValidator(QObject *parent = nullptr):QValidator(parent){}
+    State validate(QString &str,int &pos)const override
+    {
+        return QValidator::Acceptable; // метод всегда принимает вводимую строку
+    }
 };
 #endif // WINDOW_H
